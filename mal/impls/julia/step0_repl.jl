d9b16ec3ad488f9f7520bb11dfc7e6b82a295e04
woclass <inkydragon@users.noreply.github.com>
2020-03-01 16:00:57 +0800

    [julia][mal-step0] passed all test

---
+++ mal/impls/julia/step0_repl.jl
@@ -0,0 +1,31 @@
+# Step 0: REPL skeleton
+
+function READ(str)
+    str
+end
+
+function EVAL(ast, env)
+    ast
+end
+
+function PRINT(exp)
+    exp
+end
+
+function rep(str)
+    str |> READ |> s->EVAL(s,"") |> PRINT
+end
+
+function main_loop()
+    PROMPT = "user> "
+
+    print(PROMPT)
+    for line in eachline(stdin)
+        ('\x04' in line) && break # ^D
+
+        line |> rep |> println
+        print(PROMPT)
+    end # while true loop end
+end
+
+main_loop()
