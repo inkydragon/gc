8e67433ad953d23e2fe2a490c73fa85b321ded32
woclass <inkydragon@users.noreply.github.com>
2020-06-04 00:00:24 +0800

    [taichi] add 红色的圆

--- python/taichi/redCircle.py
+++ python/taichi/redCircle.py
@@ -5,19 +5,25 @@ import time
 # ti.init(debug=True, arch=ti.cpu)
 ti.init(arch=ti.gpu)
 
-GUI_TITLE = "Empty"
+GUI_TITLE = "Red Circle"
 w, h = wh = (640, 360)
+# w, h = wh = (360, 640)
 pixels = ti.Vector(3, dt=ti.f32, shape=wh)
 iResolution = ti.Vector([w, h])
+
 
 @ti.func
 def mainImage(iTime: ti.f32, i: ti.i32, j: ti.i32):
     fragCoord = ti.Vector([i, j])
-    uv = fragCoord / iResolution
-    uv_xyx = ti.Vector([uv[0], uv[1], uv[0]])
-    col = 0.5 + 0.5 * ti.cos(iTime + uv_xyx + ti.Vector([0, 2, 4]))
-    fragColor = col
+    uv = (2.0 * fragCoord - wh) / min(w, h)
+    len = uv.norm()
+
+    fragColor = ti.Vector([0.0, 0.0, 0.0])
+    if len <= 0.5:
+        fragColor = ti.Vector([1.0, 0.0, 0.0])
+
     return fragColor
+
 
 @ti.kernel
 def render(t: ti.f32):
@@ -26,19 +32,19 @@ def render(t: ti.f32):
 
     return
 
+
 def main(output_img=False):
     gui = ti.GUI(GUI_TITLE, res=wh)
     for ts in range(1000000):
         if gui.get_event(ti.GUI.ESCAPE):
             exit()
-            
+
         render(ts * 0.03)
         gui.set_image(pixels.to_numpy())
         if output_img:
             gui.show(f'frame/{ts:04d}.png')
         else:
             gui.show()
-            
 
 
 if __name__ == '__main__':
