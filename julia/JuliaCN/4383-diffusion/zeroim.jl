3acb172ca3e4a9de26212fc599c62d143386989a
woclass <inkydragon@users.noreply.github.com>
2020-09-14 16:43:29 +0800

    [julia] 虚部置零新方法

--- julia/JuliaCN/4383-diffusion/zeroim.jl
+++ julia/JuliaCN/4383-diffusion/zeroim.jl
@@ -4,6 +4,10 @@ function zeroim(arr::Array{Complex{Float64},3})
     complex(real(arr))
 end
 
+function zeroim!(arr::Array{Complex{Float64},3})
+    @. arr = complex(real(arr))
+end
+
 function zeroim!(res::Array{T, N}, arr::Array{T, N}) where {T<:Complex, N}
     map!(c->c.re+0im, res, arr)
 end
@@ -15,11 +19,13 @@ end
 big_carr = zeros(ComplexF64, 10^4,10^3,3)
 res = zeros(ComplexF64, 10^4,10^3,3)
 #=
- 220.013 ms (4 allocations: 686.65 MiB)
-  85.139 ms (0 allocations: 0 bytes)
-  85.278 ms (0 allocations: 0 bytes)
+204.392 ms (4 allocations: 686.65 MiB)
+  60.003 ms (0 allocations: 0 bytes)
+  87.327 ms (0 allocations: 0 bytes)
+  85.008 ms (0 allocations: 0 bytes)
 =#
 
 @btime zeroim($big_carr)
+@btime zeroim!($big_carr)
 @btime zeroim!($res, $big_carr)
 @btime zeroim1!($res, $big_carr)
