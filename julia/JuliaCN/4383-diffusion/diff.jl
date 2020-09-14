06076cc0019c350e171fc0518a589e53b8097e40
woclass <inkydragon@users.noreply.github.com>
2020-09-14 13:38:43 +0800

    [julia] 使用类型别名
    26.033 s (25301 allocations: 178.27 MiB)

--- julia/JuliaCN/4383-diffusion/diff.jl
+++ julia/JuliaCN/4383-diffusion/diff.jl
@@ -3,33 +3,33 @@ using FFTW
 using CUDA
 using BenchmarkTools
 
+const Arr3{T} = Array{T, 3}
+const cmA3{T} = Array{Complex{T}, 3}
+const cuA3{T} = CuArray{Complex{T}, 3}
 
 """计算参数（不可变）
 
 若要修改某些参数则应使用
 
-```julia
-mutable struct _StructName
-    # ...
-end
-```
+    mutable struct
+
 """
-struct Params
+struct Params{T}
     # 迭代次数
     iter_times :: Int64
     # 各方向维度
     dim :: NTuple{3, Int64}
 
-    M       :: Float64
+    M       :: T
     # not use
-    v_gb    :: Float64
-    δx      :: Float64
-    δt      :: Float64
-    l_gb    :: Float64
-    σ_gb    :: Float64
+    v_gb    :: T
+    δx      :: T
+    δt      :: T
+    l_gb    :: T
+    σ_gb    :: T
 
-    k2 :: Array{Float64,3}
-    f_def :: Array{Float64,3}
+    k2 :: Arr3{T}
+    f_def :: Arr3{T}
 end
 Broadcast.broadcastable(p::Params) = Ref(p)
 
@@ -37,27 +37,27 @@ Broadcast.broadcastable(p::Params) = Ref(p)
 
 使用 SoA (Struct of Array)
 """
-struct Var{T, N}
-    η_rex   :: Array{T, N}
-    cη_rex  :: Array{Complex{T}, N}
-    cuη_rex :: CuArray{Complex{T}, N}
+struct Var{T}
+    η_rex   :: Arr3{T}
+    cη_rex  :: cmA3{T}
+    cuη_rex :: cuA3{T}
 
-    η_def   :: Array{T, N}
-    cη_def  :: Array{Complex{T}, N}
-    cuη_def :: CuArray{Complex{T}, N}
+    η_def   :: Arr3{T}
+    cη_def  :: cmA3{T}
+    cuη_def :: cuA3{T}
 
-    tmp     :: Array{Complex{T}, N}
-    cu_tmp  :: CuArray{Complex{T}, N}
+    tmp     :: cmA3{T}
+    cu_tmp  :: cuA3{T}
 
-    sumetasqu   :: Array{T, N}
-    f_def_force :: Array{T, N}
+    sumetasqu   :: Arr3{T}
+    f_def_force :: Arr3{T}
 
     function Var(
-            η_rex::Array{T, N}, η_def::Array{T, N}, 
-            sumetasqu::Array{T, N}, f_def_force::Array{T, N}
-        ) where {T <: Real, N}
+            η_rex::Arr3{T}, η_def::Arr3{T}, 
+            sumetasqu::Arr3{T}, f_def_force::Arr3{T}
+        ) where {T <: Real}
         dim = size(η_rex)
-        new{T, N}(
+        new{T}(
             η_rex, complex(η_rex), CuArray{Complex{T}}(η_rex),
             η_def, complex(η_def), CuArray{Complex{T}}(η_def),
             zeros(Complex{T}, dim), CUDA.zeros(Complex{T}, dim),
@@ -124,7 +124,7 @@ function init_params(iter_times = 100, dim = (512, 512, 1))
 
     (
         Var(η_rex, η_def, sumetasqu, f_def_force),
-        Params(iter_times, dim, M, v_gb, δx,δt, l_gb,σ_gb, k2, f_def)
+        Params{Float64}(iter_times, dim, M, v_gb, δx,δt, l_gb,σ_gb, k2, f_def)
     )
 end
 
@@ -139,32 +139,32 @@ help_kernel1(η1::T, η2::T,
     (6 * p.σ_gb/p.l_gb * (η1^3 - η1 + 3*η1*η2^2) - 
      2 * η1 * η2^2 / sumetasqu^2 * f_def_force)
 
-help_kernel1!(tmp::Array{T2, N},
-    η1::Array{T1, N}, η2::Array{T1, N},
-    sumetasqu::Array{T1, N}, f_def_force::Array{T1, N},
-    p::Params,
-) where {T1<:Real, T2<:Complex, N} =
+help_kernel1!(tmp::cmA3{T},
+    η1::Arr3{T}, η2::Arr3{T},
+    sumetasqu::Arr3{T}, f_def_force::Arr3{T},
+    p::Params{T},
+) where {T<:Real} =
     @. tmp = help_kernel1(η1, η2, sumetasqu, f_def_force, p)
 
 help_kernel2(tmp::T, cη1::T, cη2::T, k2::Real, p::Params) where {T <: Complex} =
     cη1 + p.δt * (-p.M * p.σ_gb * k2 * cη2 + tmp)
 
-help_kernel2!(tmp::Array{T, N},
-    cη1::Array{T, N}, cη2::Array{T, N},
+help_kernel2!(tmp::Arr3{T},
+    cη1::Arr3{T}, cη2::Arr3{T},
     p::Params,
 ) where {T<:Complex, N} =
     @. tmp = help_kernel2(tmp, cη1, cη2, p.k2, p)
 
 "复制 src 的实数部分到 des"
-copyre!(des::Array{T, N}, src::Array{Complex{T}, N}) where {T<:Real, N} =
+copyre!(des::Arr3{T}, src::cmA3{T}) where {T<:Real, N} =
     map!(real, des, src)
 
 function help_kernel0!(
-        η1::Array{T1, N}, cη1::Array{T2, N},
-        η2::Array{T1, N}, cη2::Array{T2, N},
-        v::Var,
-        p :: Params,
-    ) where {T1<:Real, T2<:Complex, N}
+        η1::Arr3{T}, cη1::cmA3{T},
+        η2::Arr3{T}, cη2::cmA3{T},
+        v::Var{T},
+        p::Params{T},
+    ) where {T<:Real}
     # 计算 tmp
     help_kernel1!(v.tmp, η1, η2, v.sumetasqu, v.f_def_force, p)
     fft!(v.tmp)
